b9229cb888cf36b9f351e24c4edf83848ac803fa
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-01-16 14:59:54 +0530

    added blogservice route

--- blogservice/app.py
+++ blogservice/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 
-@app.get('/')
-async def blogpost():
-    return "Hello World"
+@app.post('/blogpost')
+async def blogpost(payload: json):
+    print(payload)
+    data = json.dumps(payload)
+    print('Blog posted : ' + json.dumps(data), flush=True)
+    return json.dumps({'success': True}), 200, {
+        'ContentType': 'application/json'}
